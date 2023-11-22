58902363c647e02956c2bb41703e19b85326a646
Max Derksen <47152807+ilumetric@users.noreply.github.com>
2023-11-22 21:14:20 +0600

    4.0.4

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     'name': 'Poly Source',
     "author": "Max Derksen, Oxicid",
-    'version': (4, 0, 3),
+    'version': (4, 0, 4),
     'blender': (4, 0, 0),
     'location': 'VIEW 3D > Top Bar',
     'category': '3D View',
